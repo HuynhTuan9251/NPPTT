3f6f433a0188995b7bf101e28d13e37460f28937
Huynh Tuan <tuanhuynh9251@gmail.com>
2026-04-05 16:30:53 +0700

    feattrue

---
+++ server.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./swagger");
+const connectDB = require("./config/db");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+// Chia sẻ io cho các controller
+app.set("io", io);
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/uploads", express.static("uploads"));
+
+// Swagger documentation
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api", require("./routes/taskRoutes"));
+app.use("/api", require("./routes/commentRoutes"));
+app.use("/api", require("./routes/logRoutes"));
+app.use("/api", require("./routes/teamRoutes"));
+app.use("/api", require("./routes/projectRoutes"));
+app.use("/api", require("./routes/userRoutes"));
+
+const startCronJobs = require("./scheduler");
+
+const PORT = 3000;
+
+startCronJobs();
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT + " with Socket.io");
+});
